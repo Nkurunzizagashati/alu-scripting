89e329c8e616daf38245f968dcde52ff3eab57d3
fabrice <f.nkurunziz@alustudent.com>
2023-03-08 08:33:31 -0800

    fecthing the number of sunscribers for a given sureddit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,6 +8,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-agent': 'Mozilla/5.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
+
     if res.status_code == 200:
         data = res.json()
         return data['data']['subscribers']
