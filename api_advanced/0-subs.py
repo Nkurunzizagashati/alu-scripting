be418de3e32c317ca642f8fbc52c4ae1b4057ea8
fabrice <f.nkurunziz@alustudent.com>
2023-03-08 08:32:04 -0800

    fecthing the number of sunscribers for a given sureddit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-"""
-    lets bring requests, and json functions
-    to our work space
-"""
+""" 0-subs.py """
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
-    """
-        this function takes:
-        1 arg : str (which is the name of
-                a subreddit
-        it returns the number of subscribers to
-        that specific subreddit
-    """
+    """ number of subscribers """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-agent': 'Mozilla/5.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
