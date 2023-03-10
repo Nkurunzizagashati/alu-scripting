5a33fca49f39170f05accd6f8629f634887b6694
fabrice <f.nkurunziz@alustudent.com>
2023-03-09 23:46:18 -0800

    fetching top_ten topics

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,49 +1,16 @@
 #!/usr/bin/python3
-"""
-    Queries the Reddit API and prints the titles
-    of the first 10 hot posts
-    listed for a given subreddit.
-"""
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """
-        Queries the Reddit API and prints the titles of the first 10 hot posts
-        listed for a given subreddit.
-
-        Args:
-            subreddit (str): The name of the subreddit to get the top 10 hot
-            posts for.
-
-        Returns:
-            None: If the subreddit is not valid, or if there was an error getting
-            the subreddit data.
-    """
-    # Set the API endpoint URL
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    # Set the request headers to include a User-Agent
+    """prints the titles of the first 10 hot posts listed in a subreddit """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
-
-    # Make the API request and get the response
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful
+    response = requests.get(url, headers=headers, Allow_redirects=False)
     if response.status_code != 200:
-        print("Error: Could not get subreddit data")
-        return None
-
-    # Extract the JSON data from the response
-    data = response.json()
-
-    # Get the list of posts
-    posts = data['data']['children']
-
-    # Print the titles of the first 10 posts
-    for i in range(10):
-        if i >= len(posts):
-            break
-        print(posts[i]['data']['title'])
-
-    return None
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data'['title'])
