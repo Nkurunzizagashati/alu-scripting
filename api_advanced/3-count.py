81edf3293c7f54e6e212724487bdc7043d09dadf
fabrice <f.nkurunziz@alustudent.com>
2023-03-10 09:13:18 -0800

    counting words

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -51,5 +51,3 @@ def count_words(subreddit, word_list, after="", count=[]):
                     print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
-
-count_words('programming', 'JavA java')
