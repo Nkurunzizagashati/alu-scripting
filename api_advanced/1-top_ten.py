a9c026fab82c6a2d1ef98aa9c3bf5c21a186cb3d
fabrice <f.nkurunziz@alustudent.com>
2023-03-09 23:56:42 -0800

    fetching top_ten topics

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""
+this code will print the top ten posts in a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed in a subreddit """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, Allow_redirects=False)
     if response.status_code != 200:
         print(None)
